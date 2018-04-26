e89b673720cd3f89da3743326d7268b564973718
springcome <fun.deviation@gmail.com>
2018-04-26 13:51:49 +0900

    cafe24 설정파일 제거

--- web.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-var server = app.listen(8001, function(){
-  res.writeHad(200, {'Content-Type': 'text/plain'});
-  res.send("Hello World");
-});
