4dfd99e1ada9534171972144a4fe79470ba7b0dd
springcome <fun.deviation@gmail.com>
2019-02-23 10:07:08 +0900

    테이블 생성 SQL문서 작성

---
+++ doc/database.sql
@@ -0,0 +1,14 @@
+--users DDL
+CREATE TABLE deviationmysql.users (
+	seq INT NOT NULL AUTO_INCREMENT,
+	user_email varchar(200) NOT NULL,
+	user_pwd varchar(200) NOT NULL,
+	token varchar(200) NULL,
+	join_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+	PRIMARY KEY (seq)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8
+COLLATE=utf8_general_ci
+COMMENT='Users manager'
+AUTO_INCREMENT=1;
