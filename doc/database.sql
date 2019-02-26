39190deaaf7ea3feadc165f023ffde0b6abfd15c
springcome <fun.deviation@gmail.com>
2019-02-26 22:46:40 +0900

    users테이블salt컬럼추가

--- doc/database.sql
+++ doc/database.sql
@@ -3,6 +3,7 @@ CREATE TABLE deviationmysql.users (
 	seq INT NOT NULL AUTO_INCREMENT,
 	user_email varchar(200) NOT NULL,
 	user_pwd varchar(200) NOT NULL,
+	salt varchar(128) NOT NULL,
 	token varchar(200) NULL,
 	join_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
 	PRIMARY KEY (seq)
