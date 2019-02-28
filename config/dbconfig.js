784d74a761feb5c9733e82ffb9c0fe691d2354af
deviation <fun.deviation@gmail.com>
2019-02-28 17:52:43 +0900

    데이터베이서 설정 다시 추가

---
+++ config/dbconfig.js
@@ -0,0 +1,12 @@
+module.exports =
+{
+  host: "*",
+  port: 3306,
+  user: "*",
+  password: "*",
+  database: "*",
+  connectLimit: 10,
+  connectTimeout: 2000,
+  canRetry: true,
+  waitForConnections: true
+}
