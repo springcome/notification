5d686b092cbcfa64b4f4bc7659a011fe0bdb09ad
springcome <fun.deviation@gmail.com>
2019-03-06 23:44:13 +0900

    ..

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,13 +1,13 @@
 module.exports =
 {
-  //host: "118.219.232.12",
-  host: "10.0.0.1"
+  host: "118.219.232.12",
+  // host: "10.0.0.1"
   port: 3306,
-  // user: "deviationmysql",
-  user: "deviationnodejs"
+  user: "deviationmysql",
+  // user: "deviationnodejs"
   password: "dark4862!",
-  // database: "deviationmysql",
-  database: "deviationnodejs"
+  database: "deviationmysql",
+  // database: "deviationnodejs"
   connectLimit: 10,
   connectTimeout: 2000,
   canRetry: true,
--- web.js
+++ web.js
@@ -1,36 +1,23 @@
-// var express = require('express');
-// var app = express();
-// var fs = require('fs');
-// var bodyParser = require('body-parser');
-// var session = require('express-session');
-// var secret = require('./crypto/secret.js');
-//
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(express.static(__dirname));
-// app.set('views', __dirname + '/view');
-// app.set('view engine', 'ejs');
-// app.engine('html', require('ejs').renderFile);
-// app.use(session({
-//   secret: secret.secret,
-//   resave: false,
-//   saveUninitialized: true
-// }));
-//
-// var router = require('./router/router') (app, fs, session);
-// var server = app.listen(8001, function() {
-//   console.log("Express server has started on Port 3000");
-// });
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var secret = require('./crypto/secret.js');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+app.set('views', __dirname + '/view');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(session({
+  secret: secret.secret,
+  resave: false,
+  saveUninitialized: true
+}));
 
-var http=require('http');
-
-var server=http.createServer(function(req,res){
-    res.end('test');
+var router = require('./router/router') (app, fs, session);
+var server = app.listen(3000, function() {
+  console.log("Express server has started on Port 3000");
 });
-
-server.on('listening',function(){
-    console.log('ok, server is running');
-});
-
-server.listen(8001);
