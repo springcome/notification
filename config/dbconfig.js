070c1e94773214583f2cac7548375fac83c00c83
springcome <fun.deviation@gmail.com>
2019-03-06 22:46:41 +0900

    Database설정변경

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,10 +1,13 @@
 module.exports =
 {
-  host: "118.219.232.12",
+  //host: "118.219.232.12",
+  host: "10.0.0.1"
   port: 3306,
-  user: "deviationmysql",
+  // user: "deviationmysql",
+  user: "deviationnodejs"
   password: "dark4862!",
-  database: "deviationmysql",
+  // database: "deviationmysql",
+  database: "deviationnodejs"
   connectLimit: 10,
   connectTimeout: 2000,
   canRetry: true,
